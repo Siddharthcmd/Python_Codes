b56bcdd9d3683697a367bb0ef949cedbcf9a1a7e
Siddharth Mishra <siddharthmishra@Siddharths-MacBook-Air.local>
2020-08-24 17:45:27 +0530

    pyton code

--- score.py
+++ score.py
@@ -5,7 +5,7 @@ Score (both inclusive)	Grade
 Between 80 and 100	A
 Between 73 and 79	B
 Between 65 and 72	C
-Between 0 and 64	D
+Between 00 and 64	D
 Any other value	Z
 Assume that the percentage score is a whole number.
 
@@ -13,18 +13,14 @@ Write a python program for the above requirement. Identify the test data and use
 
 '''
 
-score=input("enter the number of student to obtain his mark:")
-if(score>=80 and score <= 100 ):
-    print("garde is: A")
-elif(score>=73 and score <= 79):
+score = input("enter the number of student to obtain his mark:")
+if(score >= 80 and score <= 100):
+    print('garde is: A')
+elif(score >= 73 and score <= 79):
     print('garde is: B')
-elif(score>=65 and score <= 72):
+elif(score >= 65 and score <= 72):
     print('garde is: B')
-elif(score>=0 and score <= 64):
-    print('garde is: B') 
+elif(score >= 0 and score <= 64):
+    print('garde is: B')
 else:
-    print('Z')       
-    
-
-
-
+    print('Z')
