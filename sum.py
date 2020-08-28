9908afbe7e9df1a15c88c91ae426666e7baa60e5
Siddharth Mishra <siddharthcmd@gmail.com>
2020-08-28 14:07:59 +0530

    python codes

--- sum.py
+++ sum.py
@@ -1,8 +1,6 @@
 # Write a Python program to find the sum of digits of a given number. E.g. Sum of number 123 will be 6
 def find_sum_of_digits(number):
-    sumof = 0
-    sumof = sum(int(digit) for digit in str(number))
-    return sumof
+    return sum(int(digit) for digit in str(number))
 
 
 num = input("enter the number:")
